6065b13c80337e795d26de1080eee6d68534106b
Mike Tamis <mike.tamis@gmail.com>
2014-09-28 19:45:09 +1300

    complete overhaul of parser
    the parser now allows for multi episodes

--- index.js
+++ index.js
@@ -1,124 +1,146 @@
-/*jslint node:true, indent:2, regexp:true, nomen:true, todo: true */
+/*jslint node:true, indent:2, regexp:true, nomen:true, todo: true, bitwise: true */
 "use strict";
 
 var path = require("path");
 var debug = require('debug')('episode-parser');
 var titleCase = require("to-title-case");
 
+var stopWords = ['season', 'mkv', 'mp4', 'avi', 'mpg', 'episode'];
+
+function parseSeason(input) {
+  var result = input.toLowerCase().match(/.*season.*?(\d{1,2})/);
+  if (result) {
+    return parseInt(result[1], 10);
+  }
+  return null;
+}
+
+
 // filename: string
 // options: object
-function parseFilename(filename, options) {
+function parseFilename(filename) {
   debug("parsing %s", filename);
-  //TODO: add multi-episode parsing
-  // ShowName.sXXeAA-CC
-  // ShowName.sXXeAA-eCC
-  // ShowName.sXXeAABBCC
-  // ShowName...XX...AA-CC
-  // ShowName.sXXeAAeBBeC
+
   var ext = path.extname(filename).toLowerCase(),
-  // the following regex should match:
-  //   Community S01E04.mp4
-  //   Community s01e04.mp4
-  //   Community 1x04.mp4
-  //   Community 1-04.mp4
-    re = /(.*)\D(\d{1,2})[ex\-](\d{1,2})/i,
-    searchResults = filename.match(re),
-    show,
+    originalFilename = filename,
+    pathSplit = filename.split("/"),
+    split,
+    foundname = false,
+    show = '',
     season,
     episode,
-    offset,
-    episodeObject = {},
-    originalFilename = filename;
+    lastEpisode,
+    i,
+    episodeObject;
   filename = path.basename(filename);
-  if (options === undefined) {
-    options = {};
+  split = filename.split(/[. \-]/);
+
+  debug("split: %s", split);
+
+  function setEpisode(number) {
+    if (!episode) {
+      episode = number;
+      lastEpisode = number;
+    } else if (Math.abs(episode - number) <= 4) {
+      if (episode > number) {
+        episode = number;
+      } else if (lastEpisode < number) {
+        lastEpisode = number;
+      }
+    }
   }
 
-  offset = options.offset || 0;
-  if (searchResults === null) {
-    debug("trying search (showname) Season (s#) Episode (e#)");
-    // this regex should match:
-    //   Community Season 1 Episode 4.mp4
-    // (case insensitive)
-    re = /(.*)Season.*?(\d{1,2}).*Episode\D*?(\d{1,2})/i;
-    searchResults = filename.match(re);
-    if (searchResults !== null) { debug("Success"); }
+  function parse(piece) {
+    if (!piece) { return; }
+
+    piece = piece.toLowerCase();
+    var number = parseInt(piece, 10),
+      seasonString = '',
+      j,
+      c,
+      epSplit = piece.split(/[e-e]/);
+    if (stopWords.indexOf(piece) >= 0) {
+      if (show) { foundname = true; }
+      return;
+    }
+
+    if (/\dx\d/.test(piece)) {
+      piece.split('x').forEach(parse);
+      if (show) { foundname = true; }
+      return;
+    }
+    if (/s\d/.test(piece)) {
+      if (show) { foundname = true; }
+
+      for (j = 1; j < piece.length; j += 1) {
+        c = piece.charAt(j);
+        if (c >= '0' && c <= '9') {
+          seasonString += c;
+        } else {
+          break;
+        }
+      }
+      season = parseInt(seasonString, 10);
+      parse(piece.substr(j, piece.length));
+    } else if (/e\d/.test(piece)) {
+      if (show) { foundname = true; }
+      for (j in epSplit) {
+        if (epSplit.hasOwnProperty(j)) {
+          setEpisode(parseInt(epSplit[j], 10));
+        }
+      }
+    } else if (!isNaN(number) && show) {
+      if (show) { foundname = true; }
+      if (episode) {
+        setEpisode(number);
+      } else if (!season) {season = number;
+        } else { setEpisode(number); }
+    } else if (!foundname) {
+      show += piece;
+      show += " ";
+    }
   }
 
-  if (searchResults === null) {
-    debug("trying search (showname)...(s#)...(e#)");
-    // this regex should match:
-    //   Community...1...4.mp4
-    re = /(.*)\.+(\d{1,2})\.+(\d{1,2})/;
-    searchResults = filename.match(re);
-    if (searchResults !== null) { debug("Success"); }
-  }
-
-  if (searchResults === null) {
-    debug("trying search (showname) (s#e##)");
-    // this regex should match:
-    //   Community 104.mp4
-    re = /(.*)\D(\d)(\d\d)\D/;
-    searchResults = filename.match(re);
-    if (searchResults !== null) { debug("Success"); }
-  }
-
-
-  if (searchResults === null && options.season) {
-    debug("trying search (showname) (e##) - if season specified");
-    // this regex should match:
-    //   Community 04.mp4
-    // but only if we've specified a season with season flag
-    re = /(.*)\D(\d+)\D/;
-    searchResults = filename.match(re);
-    if (searchResults !== null) { debug("Success"); }
-  }
-
-  if (searchResults === null && options.season && options.show) {
-    debug("trying search (e##) - if season specified and show specified");
-    // this regex should match:
-    //   04.mp4
-    // but only if we've specified a season and show with flags
-    re = /(\d+)\D/;
-    searchResults = filename.match(re);
-    if (searchResults !== null) { debug("Success"); }
-  }
-
-  try {
-    show = options.show || searchResults[1];
-  } catch (e) {
-    debug("failed to parse: %s", filename);
-    return null;
+  for (i in split) {
+    if (split.hasOwnProperty(i)) {
+      parse(split[i]);
+    }
   }
   show = titleCase(show
       // remove hanging characters
       .replace(/^[\-.\s]+|[\-.\s]+$/g, "")
       .trim());
 
-  if (options.episode) {
-    episode = options.episode + offset;
-    if (searchResults !== null) {
-      searchResults.pop();
-    }
-  } else {
-    try {
-      episode = Number(searchResults.pop()) + offset;
-    } catch (e) {
-      debug("failed to parse: %s", filename);
-      return null;
+  if (season > 101) {
+    lastEpisode = episode = season % 100;
+    season = season / 100 | 0;
+  }
+  if (season && !episode) {
+    setEpisode(season);
+    season = null;
+  }
+
+  if (!season) {
+    if (pathSplit.length >= 2) {
+      season = parseSeason(pathSplit[pathSplit.length - 2]);
     }
   }
 
-  season = options.season || Number(searchResults.pop());
-  //TODO: extract quality https://github.com/echel0n/SickRage/wiki/Quality-Settings
+  if (!episode || !season) {
+    return null;
+  }
+
   episodeObject = {
     originalFilename: originalFilename,
-    show: show,
+    SeriesName: show,
     season: season,
     episode: episode,
+    lastEpisode: lastEpisode,
     extension: ext
   };
+  debug("result: %s", JSON.stringify(episodeObject));
   return episodeObject;
 }
+
 
 module.exports =  parseFilename;
